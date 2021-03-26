874acb61a0e1ad880c216f966b99cccce41a5214
unknown <ccodava@gmail.com>
2021-03-26 09:15:01 +0530

    second commit

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,41 +1,114 @@
 import React, { Component } from "react";
+import './Login.css';
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: null,
-      password: null,
+      username: "",
+      password: "",
+      error: "",
+      output: "",
+      sclicked: false,
+      lclicked: true,
     };
 
-    this.change=this.change.bind(this);
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.submit = this.submit.bind(this);
+    this.slide = this.slide.bind(this);
   }
 
-
-  submit(evt){
-    evt.preventDefault();
-  }
-
-
-  change(evt){
-    evt.preventDefault();
+  handleUserChange(e) {
     this.setState({
-        username: this.target.name,
+      username: e.target.value,
     });
+  }
+
+  handlePasswordChange(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+
+  submit(e) {
+    e.preventDefault();
+    if (!this.state.username || this.state.username !== "ravi") {
+      return this.setState({ error: "Invalid username" });
+    } else if (!this.state.password || this.state.password !== "12345") {
+      return this.setState({ error: "Invalid password" });
+    } else {
+      return (
+        this.setState({ output: "logged in successfully" }),
+        this.setState({ error: "" })
+      );
+    }
+  }
+
+  slide(){
+    if(this.state.sclicked === false){
+      this.setState(
+        {
+          sclicked: true,
+          lclicked: false
+        })
+    }else{
+      this.setState({
+      sclicked: false,
+      lclicked: true
+      })
+    }
   }
 
   render() {
     return (
-      <div>
 
-          <h1> user is {this.state.username}</h1>
-        <form onSubmit={this.submit}>
-          <input type="text" name="uname" value="" onChange={this.change}/>
+<>
+<h1> {this.state.output}</h1>
+        {this.state.error && <h3>{this.state.error}</h3>}
+      <div className="form-structor" >
 
-          <input type="password" name="password" value="" />
-          <input type="submit" value="Submit" />
-        </form>
+        <div className={this.state.sclicked ? 'signup slide-up':'signup' }>
+          <h2 className="form-title" id="signup" onClick={this.slide}>
+            <span>or</span>Sign up
+          </h2>
+          <div className="form-holder">
+            <input type="text" className="input" placeholder="Name" />
+            <input type="email" className="input" placeholder="Email" />
+            <input type="password" className="input" placeholder="Password" />
+          </div>
+          <button className="submit-btn">Sign up</button>
+        </div>
+        <div className={this.state.lclicked? "login slide-up" : "login"}>
+          <div className="center">
+            <h2 className="form-title" id="login" onClick={this.slide}>
+              <span>or</span>Log in
+            </h2>
+            <div className="form-holder">
+            <input
+            type="text"
+            className="input"
+            name="username"
+            placeholder="Name" 
+            value={this.state.username}
+            onChange={this.handleUserChange}
+            autoComplete="on"/>
+
+          <input
+            type="text"
+            className="input"
+            name="password"
+            placeholder="Password" 
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+            autoComplete="on"
+          />
+            </div>
+            <button className="submit-btn"  onClick={this.submit}>Log in</button>
+          </div>
+        </div>
       </div>
+      </>
     );
   }
 }
---
+++ src/Component/Static-pages/About.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Contact.css'
+
+export default function About(){
+    return(
+        <div className='contact'>
+            <p>About</p>
+        </div>
+    );
+} 
